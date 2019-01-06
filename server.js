9a7f6049ba6120433fc5af220eddcfe6c2a6c595
Shehzan Devani <shehzan@shehzans-mbp.lan>
2019-01-06 14:26:53 -0800

    Add documentation for creating the start game endpoint.

--- server.js
+++ server.js
@@ -77,22 +77,30 @@ app.get("/api/game/:gameId/players", (req, res) => {
 });
 
 app.post("/api/game/:gameId/start", (req, res) => {
+	// These are the steps we need to implement
 	// 1. Create a deck
 	// 2. Create N number of player hands
 	// 3. Update number of cards for playerhands and deck
-	// 4. Pick a player to go first
+	// 4. Pick a player to go first (Still need to do this)
 	// 5. Return
 	//      - Deck
 	//      - Playerhands
+
+	// We create a promise to find our game. We chain that to a promise to find
+	// all of the players for this game. We store this promise in a variable.
+	// To use "playersPromise", you can call .then on it and the argument will be
+	// players.
 	let playersPromise = Game.findOne({
 		where: {
 			id: req.params.gameId
 		}
 	}).then((game) => {
-		console.log("Game", game);
 		return game.getPlayers()
 	});
 
+	// Once we have all of the players, we create a deck. We need players in order
+	// to set the proper cardCount.
+	// We store this promise in a variable as well.
 	let deckCreationPromise = playersPromise.then((players) => {
 		return Deck.create({
 			gameId: req.params.gameId,
@@ -100,8 +108,11 @@ app.post("/api/game/:gameId/start", (req, res) => {
 		})
 	});
 
+	// We attach a second .then statement to playersPromise to create all of the
+	// players' hands.
 	let playerInfoPromises = playersPromise.then((players) => {
-		console.log("Players", players);
+		// Because we need to create a hand for each player, we have to iterate through
+		// the players array. We use .map in order to track each generated promise.
 		let handCreationPromises = players.map((player) => {
 			let handPromise = Hand.create({
 				gameId: req.params.gameId,
@@ -109,20 +120,37 @@ app.post("/api/game/:gameId/start", (req, res) => {
 				playerId: player.id
 			});
 
+			// We want to associate the player and hand, so rather than just returning
+			// handPromise, we create an object and return the promise of those two.
+			// The result of this promise will be in the format presented below.
 			return Promise.props({
 				player: player,
 				hand: handPromise
 			});
 		});
-		
-		return Promise.props(handCreationPromises);
+
+		// This returns the promise that is an accumulation of each player's
+		// "handeCreationPromise".
+		return Promise.all(handCreationPromises);
 	});
 
+	// We create one final promise so that all of the DB changes we've made can finish.
+	// Once the deck is created and the playerInfo is ready, we can return the results.
+	// The format of the result will be:
+	// {
+	// 	deck: DeckModel,
+	// 	playerInfo: [
+	// 		{
+	// 			player: playerModel,
+	// 			hand: handModel
+	// 		},
+	// 		...
+	// 	]
+	// }
 	return Promise.props({
 		playerInfo: playerInfoPromises,
 		deck: deckCreationPromise
 	}).then((result) => {
-		console.log("Result", result);
 		res.json(result);
 	});
 });
